41a12ff9725474d3ea2c5ccdb9becdc2ce0a44fc
set <happeningzp@gmail.com>
2023-09-28 13:01:37 +0300

    update migrations

--- database/migrations/2023_09_19_172901_create_tasks_table.php
+++ database/migrations/2023_09_19_172901_create_tasks_table.php
@@ -14,11 +14,10 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('parent_id')->nullable()->constrained('tasks')->onDelete('cascade');
-//            $table->foreignId('task_id')->nullable()->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained();
             $table->string('title', 255);
             $table->text('description')->nullable();
-            $table->enum('status', ['todo', 'done'])->default('todo');
+            $table->string('status', 64)->default('todo');
             $table->smallInteger('priority');
             $table->timestamp('completed_at')->nullable();
             $table->timestamps();
